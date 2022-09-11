da9e404b6a3a54e070785e23d579cd0a0c33edc2
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2022-09-11 12:47:26 +0300

    Update data.js

--- src/mockBackend/data.js
+++ src/mockBackend/data.js
@@ -5,7 +5,7 @@ When building user interfaces, it can be helpful to
 mock out simplified versions of the data that our code
 will eventually fetch from backend servers 
 */
-
+// eslint-disable-next-line
 export default {
     "/categories": ["Shirts", "Pants", "Shoes", "Accessories"],
     "/items?category=Shirts": ["T-Shirts", "Casual", "Formal"],
